eb516dc490716bd9d0febb106ff125f37aeb423d
fahrilmoonstar-svg <fahrilmoonstar@gmail.com>
2026-08-09 21:57:52 +0800

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,17 @@
+#KALKULATOR SEDERHANA PYTHON
+
+#input user
+a = int(input("Angka A : "))
+b = int(input("Angka B : "))
+
+#Rumus
+tambah = a + b
+kurang = a - b
+kali = a * b
+bagi = a / b
+
+#output / hasil
+print(f"Hasil {a} + {b} = {tambah}")
+print(f"Hasil {a} - {b} = {kurang}")
+print(f"Hasil {a} * {b} = {kali}")
+print(f"Hasil {a} / {b} = {bagi}")
